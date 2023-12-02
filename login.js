f2aa1a790623db9ebc06868943f18472a561928e
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2023-12-02 14:36:14 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 // Cole suas configurações do Firebase aqui
 const firebaseConfig = {
-   apiKey: "AIzaSyDb7m40G6cT8zZ0tqxmkyQ78OWBHy8w9LI",
-    authDomain: "pecaqui-cc6c1.firebaseapp.com",
-    projectId: "pecaqui-cc6c1",
-    storageBucket: "pecaqui-cc6c1.appspot.com",
-    messagingSenderId: "502925766451",
-    appId: "1:502925766451:web:52473d00da34033f789846"
-  };
+  apiKey: "AIzaSyDb7m40G6cT8zZ0tqxmkyQ78OWBHy8w9LI",
+  authDomain: "pecaqui-cc6c1.firebaseapp.com",
+  projectId: "pecaqui-cc6c1",
+  storageBucket: "pecaqui-cc6c1.appspot.com",
+  messagingSenderId: "502925766451",
+  appId: "1:502925766451:web:52473d00da34033f789846"
+};
 
 // Inicialize o Firebase
 firebase.initializeApp(firebaseConfig);
@@ -21,24 +21,41 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  signInWithEmailPassword(email, password);
+  // Verifica se o dispositivo está conectado à internet
+  if (navigator.onLine) {
+    // Chama o método `signInWithEmailPassword()`
+    signInWithEmailPassword(email, password);
+  } else {
+    // Exiba uma mensagem de erro
+    alert("O dispositivo não está conectado à internet.");
+  }
 });
 
 // Função de autenticação por email e senha
 function signInWithEmailPassword(email, password) {
+  // Chama o método `signInWithEmailAndPassword()` do Firebase
   auth.signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Usuário autenticado com sucesso
       const user = userCredential.user;
       console.log(user);
-      window.location.href = 'home.html'; // Redirecionamento após o login bem-sucedido
+      // Redirecionamento após o login bem-sucedido
+      window.location.href = 'home.html';
     })
     .catch((error) => {
       // Tratamento de erro
       const errorCode = error.code;
       const errorMessage = error.message;
       console.error(errorMessage);
-    });
 
-    signInWithEmailPassword(email, password);
-};
+      // Verifica se o erro é de conexão com a internet
+      if (errorCode === "auth/network-request-failed") {
+        // Exibe uma mensagem de erro personalizada
+        alert("O dispositivo não está conectado à internet.");
+      } else {
+        // Exibe a mensagem de erro padrão do Firebase
+        alert(errorMessage);
+      }
+    });
+}
+
