e6221cc30d85abb2e0b0bed971379a3d850fc6b2
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2023-12-09 20:57:24 -0300

    Update guarda.js

--- guarda.js
+++ guarda.js
@@ -1,3 +1,13 @@
+// Inicializa o Firebase App
+firebase.initializeApp({
+  apiKey: "AIzaSyDb7m40G6cT8zZ0tqxmkyQ78OWBHy8w9LI",
+  authDomain: "pecaqui-cc6c1.firebaseapp.com",
+  projectId: "pecaqui-cc6c1",
+  storageBucket: "pecaqui-cc6c1.appspot.com",
+  messagingSenderId: "502925766451",
+  appId: "1:502925766451:web:52473d00da34033f789846"
+});
+
 firebase.auth().onAuthStateChanged(user => {
     if (!user) {
         window.location.href = "index.html";
