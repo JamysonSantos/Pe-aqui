91e93dee815c15dc62a08cc098ffd9ca6f01f3d8
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2023-12-09 20:15:00 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,3 @@
-// Importa a biblioteca JavaScript do Firebase
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js");
-
 // Inicializa o Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyDb7m40G6cT8zZ0tqxmkyQ78OWBHy8w9LI",
@@ -11,6 +7,10 @@ firebase.initializeApp({
   messagingSenderId: "502925766451",
   appId: "1:502925766451:web:52473d00da34033f789846"
 });
+
+// Importa a biblioteca JavaScript do Firebase
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js");
 
 // Observa o estado de autenticação do Firebase
 firebase.auth().onAuthStateChanged(user => {
