98c20b9b8b0f2515145a9c1bdb33158d96c391b6
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-01-11 21:48:09 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -115,7 +115,7 @@ function createUserWithEmailAndPassword(companyName, email, password) {
         alert(errorMessage);
       }
     });
-}
+	
 // Função para validar um e-mail
 function validateEmail(email) {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
