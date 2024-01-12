814b03852402530880be35474699c9dec40d3dab
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-01-11 22:14:24 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -91,86 +91,10 @@ document.getElementById('signupForm').addEventListener('submit', function (event
     // Exiba uma mensagem de erro
     alert("O dispositivo não está conectado à internet.");
   }
-
-// Função para criar um usuário com e-mail e senha
-function createUserWithEmailAndPassword(companyName, email, password, ownerName, contactNumber, addressLine, neighborhood, cityState) {
-  // Valida o nome da empresa
-  if (!companyName || companyName.trim().length === 0) {
-    alert("O campo 'Nome da sua empresa' é obrigatório.");
-    return;
-  }
-
-  // Valida o e-mail
-  if (!email || email.trim().length === 0) {
-    alert("O campo 'E-mail' é obrigatório.");
-    return;
-  }
-
-  // Valida a senha
-  if (!password || password.trim().length === 0) {
-    alert("O campo 'Crie sua Senha' é obrigatório.");
-    return;
-  }
-
-  // Valida o formato do e-mail
-  if (!validateEmail(email)) {
-    alert("O e-mail fornecido é inválido.");
-    return;
-  }
-
-  // Cria um objeto com os dados da empresa
-  const empresaData = {
-    nomeEmpresa: companyName,
-    email: email,
-    proprietario: ownerName,
-    'numero de contato': contactNumber,
-    'rua e numero': addressLine,
-    bairro: neighborhood,
-    'Cidade/Estado': cityState,
-    'Crie sua Senha': password,
-  };
-
-  // Chama o método `createUserWithEmailAndPassword()` do Firebase
-  auth.createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Usuário criado com sucesso
-      const user = userCredential.user;
-      console.log(user);
-
-      // Atualiza o nome da empresa no perfil do usuário
-      return user.updateProfile({
-        displayName: companyName
-      });
-    })
-    .then(() => {
-      // Adiciona um novo documento à coleção "Empresas"
-      return db.collection("Empresas").add(empresaData);
-    })
-    .then(() => {
-      // Redirecionamento após o cadastro bem-sucedido
-      window.location.href = 'poscadastro.html';
-    })
-    .catch((error) => {
-      // Tratamento de erro
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(errorMessage);
-
-      // Verifica se o erro é de conexão com a internet
-      if (errorCode === "auth/network-request-failed") {
-        // Exibe uma mensagem de erro personalizada
-        alert("O dispositivo não está conectado à internet.");
-      } else {
-        // Exibe a mensagem de erro padrão do Firebase
-        alert(errorMessage);
-      }
-    });
-}
+});
 
 // Função para validar um e-mail
 function validateEmail(email) {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(email);
 }
-
-
