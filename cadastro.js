aef4452a1f22fea7e912ec2d61f198060d20c3e8
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-01-11 21:45:40 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -120,4 +120,4 @@ function createUserWithEmailAndPassword(companyName, email, password) {
 function validateEmail(email) {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(email);
-});
+}
