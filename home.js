6e7acad33875e207dfb008e996763dc51720ef35
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-01-25 21:40:37 -0300

    Update home.js

--- home.js
+++ home.js
@@ -54,15 +54,15 @@ firebase.auth().onAuthStateChanged(user => {
             // Cria um canvas para redimensionar a imagem
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
-            canvas.width = 240;
-            canvas.height = 427;
+            canvas.width = 360;
+            canvas.height = 640;
 
             // Cria uma nova imagem no canvas com as dimensões desejadas
             const image = new Image();
             image.src = imgSrc;
 
             // Desenha a imagem no canvas
-            ctx.drawImage(image, 0, 0, 240, 427);
+            ctx.drawImage(image, 0, 0, 360, 640);
 
             // Obtém a versão redimensionada da imagem como um data URL
             const resizedImgSrc = canvas.toDataURL('image/jpeg', 0.7); // Qualidade JPEG de 70%
