7ddf5f3b16d9b4fc095693461dc0d8c9c1f3a74f
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-03-25 23:08:14 -0300

    Update cadastrocardapio.js

--- cadastrocardapio.js
+++ cadastrocardapio.js
@@ -15,114 +15,85 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-// Função para adicionar um novo item ao cardápio
-function addItem() {
-    // Obter o ID do usuário atualmente autenticado
-    const userId = firebase.auth().currentUser.uid;
+// Array de categorias e itens
+    const categories = {};
 
-    // Obter os valores dos campos do formulário
-    const nomeItem = document.getElementById('item-nome').value.trim();
-    const descricaoItem = document.getElementById('item-descricao').value.trim();
-    const precoItem = document.getElementById('price').value.trim();
-    const categoriaItem = document.getElementById('item-categoria').value.trim();
+    // Adiciona evento ao botão "Adicionar"
+    document.querySelector('.bg-red-700').addEventListener('click', function() {
+        const name = document.querySelector('input[placeholder="Nome do item"]').value;
+        const description = document.querySelector('textarea').value;
+        const price = document.querySelector('input[placeholder="Preço"]').value;
+        const category = document.getElementById('categoryInput').value;
+        const image = document.querySelector('input[type="file"]').files[0]; // Captura o arquivo de imagem selecionado
 
-    // Verificar se os campos obrigatórios estão preenchidos
-    if (!nomeItem || !descricaoItem || !precoItem || !categoriaItem) {
-        alert('Por favor, preencha todos os campos.');
-        return;
-    }
-
-    // Obter referência para a coleção "cardapio" do usuário atual
-    const cardapioRef = db.collection('Empresas').doc(userId).collection('cardapio');
-
-    // Verifica se o usuário selecionou alguma imagem
-    const fileInput = document.getElementById('fileInput');
-    if (fileInput.files.length === 0) {
-        // Se nenhum arquivo foi selecionado, salva o item sem imagem
-        salvarItemSemImagem(cardapioRef, nomeItem, descricaoItem, precoItem, categoriaItem);
-    } else {
-        // Se o usuário selecionou uma ou mais imagens, salva o item com as imagens
-        salvarItemComImagens(cardapioRef, nomeItem, descricaoItem, precoItem, categoriaItem, fileInput.files);
-    }
-}
-
-// Função para salvar um item sem imagem no Firestore
-function salvarItemSemImagem(cardapioRef, nomeItem, descricaoItem, precoItem, categoriaItem) {
-    cardapioRef.add({
-        nome: nomeItem,
-        descricao: descricaoItem,
-        preco: precoItem,
-        categoria: categoriaItem,
-        imagens: [] // Array vazio para armazenar URLs das imagens
-    })
-    .then(() => {
-        console.log('Item cadastrado com sucesso');
-        limparCamposDoFormulario();
-    })
-    .catch((error) => {
-        console.error('Erro ao cadastrar item:', error);
+        const menuItem = { name, description, price, category, image };
+        // Verifica se a categoria já existe no objeto categories
+        if (!categories[category]) {
+            categories[category] = [];
+        }
+        // Adiciona o item à categoria correspondente
+        categories[category].push(menuItem);
+        // Limpa os campos do formulário
+        document.querySelectorAll('input, textarea').forEach(input => input.value = '');
+        // Atualiza a exibição dos itens
+        displayMenuItems();
     });
-}
 
-// Função para salvar um item com imagens no Firestore e armazená-las no Firebase Storage
-function salvarItemComImagens(cardapioRef, nomeItem, descricaoItem, precoItem, categoriaItem, imagens) {
-    // Salva as imagens no Firebase Storage
-    const imagensUrls = [];
-    const promises = [];
-    imagens.forEach((imagem) => {
-        const storageRef = firebase.storage().ref().child(`usuarios/${userId}/cardapio/${imagem.name}`);
-        const uploadTask = storageRef.put(imagem);
-        promises.push(
-            new Promise((resolve, reject) => {
-                uploadTask.on('state_changed', null, reject, () => {
-                    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-                        imagensUrls.push(downloadURL);
-                        resolve();
-                    });
-                });
-            })
-        );
-    });
-    
-    // Após todas as imagens serem carregadas, salva o item no Firestore
-    Promise.all(promises).then(() => {
-        cardapioRef.add({
-            nome: nomeItem,
-            descricao: descricaoItem,
-            preco: precoItem,
-            categoria: categoriaItem,
-            imagens: imagensUrls // URLs das imagens carregadas
-        })
-        .then(() => {
-            console.log('Item cadastrado com sucesso');
-            limparCamposDoFormulario();
-        })
-        .catch((error) => {
-            console.error('Erro ao cadastrar item:', error);
+    // Função para exibir os itens do cardápio
+    function displayMenuItems() {
+        const menuItemsContainer = document.getElementById('menuItems');
+        menuItemsContainer.innerHTML = ''; // Limpa o conteúdo existente
+        // Percorre as categorias e exibe os itens
+        Object.keys(categories).forEach(category => {
+            const categoryTitle = document.createElement('h2');
+            categoryTitle.textContent = category.toUpperCase();
+            categoryTitle.classList.add('text-xl'); // Adiciona classe de estilo para o título da categoria
+            menuItemsContainer.appendChild(categoryTitle); // Adiciona título da categoria
+            categories[category].forEach(item => {
+                const newItem = document.createElement('div');
+                newItem.innerHTML = `
+                    <div class="border p-4 mb-2 rounded-lg">
+                        <h3 class="text-lg font-bold">${item.name}</h3>
+                        <p>${item.description}</p>
+                        <p class="item-price">R$ ${item.price}</p>
+                        <img src="${item.image ? URL.createObjectURL(item.image) : ''}" alt="${item.name}" class="w-20 h-20 object-cover rounded-lg mt-2">
+                    </div>
+                `;
+                menuItemsContainer.appendChild(newItem); // Adiciona item do cardápio
+            });
         });
-    }).catch((error) => {
-        console.error('Erro ao carregar imagens:', error);
+    }
+
+    // Adiciona evento ao campo de categorias
+    const categoryInput = document.getElementById('categoryInput');
+    categoryInput.addEventListener('input', function() {
+        const inputValue = this.value.toLowerCase();
+        const suggestedCategoriesContainer = document.getElementById('suggestedCategories');
+        // Limpa as categorias sugeridas anteriores
+        suggestedCategoriesContainer.innerHTML = '';
+        // Filtra e exibe as categorias sugeridas que correspondem ao texto digitado
+        Object.keys(categories).forEach(category => {
+            if (category.toLowerCase().includes(inputValue)) {
+                const categoryElement = document.createElement('div');
+                categoryElement.textContent = category;
+                categoryElement.classList.add('cursor-pointer', 'hover:bg-gray-300', 'rounded-lg', 'p-1', 'mb-1');
+                categoryElement.addEventListener('click', function() {
+                    categoryInput.value = category;
+                    suggestedCategoriesContainer.classList.add('hidden');
+                });
+                suggestedCategoriesContainer.appendChild(categoryElement);
+            }
+        });
+        // Exibe as categorias sugeridas se houver algum texto digitado
+        if (inputValue.trim() !== '') {
+            suggestedCategoriesContainer.classList.remove('hidden');
+        } else {
+            suggestedCategoriesContainer.classList.add('hidden');
+        }
     });
-}
 
-// Função para limpar os campos do formulário após o cadastro
-function limparCamposDoFormulario() {
-    document.getElementById('item-nome').value = '';
-    document.getElementById('item-descricao').value = '';
-    document.getElementById('price').value = '';
-    document.getElementById('item-categoria').value = '';
-    document.getElementById('fileInput').value = ''; // Limpa a seleção de arquivos
-}
-
-// Função para manipular a mudança de arquivos (se o usuário estiver anexando imagens)
-function onFileChange(event) {
-    // Adicione o código para manipular a mudança de arquivos aqui
-}
-
-// Função para anexar imagens aos itens do cardápio
-function attachImage() {
-    // Adicione o código para anexar imagens aqui
-}
+    // Exibe os itens do cardápio ao carregar a página
+    displayMenuItems();
 
 
 
